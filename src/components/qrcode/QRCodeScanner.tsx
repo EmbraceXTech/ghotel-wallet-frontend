e5f8fda49c1c81c07933bc8658a007ced192faf1
ribbinpo <ribbinpo@gmail.com>
2024-01-21 16:47:39 +0700

    feat: :sparkles: update pay ui

--- src/components/qrcode/QRCodeScanner.tsx
+++ src/components/qrcode/QRCodeScanner.tsx
@@ -7,6 +7,7 @@ export default function QRCodeScanner() {
       onDecode={(result) => console.log(result)}
       onError={(error) => console.log(error?.message)}
       stopDecoding={false}
+      containerStyle={{ borderRadius: 12 }}
     />
   );
 }
--- src/views/pay.view.tsx
+++ src/views/pay.view.tsx
@@ -2,7 +2,8 @@ import { QRCodeScanner } from "@/components/qrcode";
 
 export default function PayView() {
   return (
-    <div className="w-full flex items-center h-full">
+    <div className="w-full flex flex-col h-full bg-white p-1 rounded-xl border-[#E9EBED]">
+      <div className="my-3 font-medium text-sm text-center">Pay</div>
       <QRCodeScanner />
     </div>
   );
